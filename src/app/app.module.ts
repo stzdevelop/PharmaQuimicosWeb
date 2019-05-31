a8d941a08f5f8c9679110d44ca2aff5f593c1f39
stzdevelop <stzdevelop@gmail.com>
2019-05-31 06:50:35 -0500

    Nav y Footer Arreglados, eliminar el modulo productos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,16 @@ import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { FooterMainComponent } from './footer-main/footer-main.component';
+import { HomeComponent } from './home/home.component';
+import { ProductosComponent } from './productos/productos/productos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMainComponent,
-    FooterMainComponent    
+    FooterMainComponent,
+    HomeComponent,
+    ProductosComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/productos/productos-detail/productos-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-productos-detail',
+  templateUrl: './productos-detail.component.html',
+  styleUrls: ['./productos-detail.component.css']
+})
+export class ProductosDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
